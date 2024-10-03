c20e9ad0fb67b17732a9d42f4bd396abf698987c
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-10-03 16:22:29 +0200

    step 8: change actions to calculate result

--- arithmetic/src/arithmetic_actions.rs
+++ arithmetic/src/arithmetic_actions.rs
@@ -6,42 +6,20 @@ pub type Input = str;
 pub type Ctx<'i> = Context<'i, Input>;
 #[allow(dead_code)]
 pub type Token<'i> = RustemoToken<'i, Input, TokenKind>;
-pub type Number = String;
+pub type Number = i32;
 pub fn number(_ctx: &Ctx, token: Token) -> Number {
-    token.value.into()
+    token.value.parse().unwrap()
 }
-#[derive(Debug, Clone)]
-pub struct Add {
-    pub left: Box<E>,
-    pub right: Box<E>,
-}
-#[derive(Debug, Clone)]
-pub struct Mul {
-    pub left: Box<E>,
-    pub right: Box<E>,
-}
-#[derive(Debug, Clone)]
-pub enum E {
-    Add(Add),
-    Mul(Mul),
-    Paren(Box<E>),
-    Num(Number),
-}
+pub type E = i32;
 pub fn e_add(_ctx: &Ctx, left: E, right: E) -> E {
-    E::Add(Add {
-        left: Box::new(left),
-        right: Box::new(right),
-    })
+    left + right
 }
 pub fn e_mul(_ctx: &Ctx, left: E, right: E) -> E {
-    E::Mul(Mul {
-        left: Box::new(left),
-        right: Box::new(right),
-    })
+    left * right
 }
 pub fn e_paren(_ctx: &Ctx, e: E) -> E {
-    E::Paren(Box::new(e))
+    e
 }
 pub fn e_num(_ctx: &Ctx, number: Number) -> E {
-    E::Num(number)
+    number
 }
